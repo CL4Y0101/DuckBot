c8d2a54fce7babcbe1180e37cda43f35cfebe992
luhnox <mferyiskandar2006@gmail.com>
2025-11-08 21:55:18 +0800

    update index.js code

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 require('dotenv').config({
   quiet: true
 });
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const { loadCommands } = require('./src/utils/commandLoader');
-const { loadEvents } = require('./src/utils/eventLoader');
+const {
+  Client,
+  GatewayIntentBits,
+  Collection
+} = require('discord.js');
+const {
+  loadCommands
+} = require('./src/utils/commandLoader');
+const {
+  loadEvents
+} = require('./src/utils/eventLoader');
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
